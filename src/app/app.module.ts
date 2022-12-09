6a6c99ee917cc35e5bbae1d32a5f1414a40a11e2
sherin200 <sherinsebastian21>
2022-12-09 14:27:22 +0530

    userlogin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLogincomplaintComponent } from './admin-logincomplaint/admin-logincomplaint.component';
+import { UserLogincomplaintComponent } from './user-logincomplaint/user-logincomplaint.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLogincomplaintComponent
+    AdminLogincomplaintComponent,
+    UserLogincomplaintComponent
   ],
   imports: [
     BrowserModule,
